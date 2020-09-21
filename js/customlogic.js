2add23f9b3161b0d4d876b96eda25379eb3e52d2
andrey <andrei.nerobow@yandex.ru>
2020-09-21 21:42:21 +0300

    formatting fix

--- js/customlogic.js
+++ js/customlogic.js
@@ -1,22 +1,18 @@
-const fireballSize=22;
-const wizardSpeed=3;
-const wizardWidth=70;
+const fireballSize = 22;
+const wizardSpeed = 3;
+const wizardWidth = 70;
 
-var getFireballSpeed=function(isleft)
-{
-if(isleft){ return 2;}
-return 5;
+var getFireballSpeed = function (isleft) {
+  if (isleft) { return 2; }
+  return 5;
 }
-var getWizardHeight= function()
-{
-    return 1.337 *wizardWidth;
+var getWizardHeight = function () {
+  return 1.337 * wizardWidth;
 }
-var getWizardX=function(fieldWidth)
-{
-return (fieldWidth- wizardWidth)/2;
+var getWizardX = function (fieldWidth) {
 
+  return (fieldWidth - wizardWidth) / 2;
 }
-var getWizardY=function(fieldHeight)
-{
-return (fieldHeight/3);
+var getWizardY = function (fieldHeight) {
+  return (fieldHeight / 3);
 }
